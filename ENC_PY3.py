d895371903d2c906ec89c073dc872280f9f47842
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-06-22 20:57:10 +0600

    Create ENC_PY3.py

---
+++ ENC_PY3.py
@@ -0,0 +1,21 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from PY3 import encryption
+ 
+        encryption()
